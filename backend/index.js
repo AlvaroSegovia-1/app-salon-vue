ab560a5a51cdea13fb8e6849d1d808f7054045f0
Alvagit25 <alvarodesegovia@gmail.com>
2025-03-30 11:10:41 +0200

    añadir conexión a bbdd

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { db } from "./config/db.js";
 import servicesRoutes from "./routes/servicesRoutes.js";
+
+// Variables de entorno
+dotenv.config();
 
 //Configurar la app
 const app = express();
+
+// Conectar a BD
+db();
 
 // Definir una ruta
 app.use("/api/services", servicesRoutes);
@@ -12,5 +21,8 @@ const PORT = process.env.PORT || 4000;
 
 // Arrancar la app
 app.listen(PORT, () => {
-  console.log("El servidor se está ejecutando en el puerto:", PORT);
+  console.log(
+    colors.blue("El servidor se está ejecutando en el puerto:"),
+    colors.red(PORT),
+  );
 });
