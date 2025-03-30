8bb6ebd1667f7ce8d98f25f773bfda2d501b3cd5
Alvagit25 <alvarodesegovia@gmail.com>
2025-03-30 09:49:04 +0200

    version-2 node

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,11 @@
 import express from "express";
+import servicesRoutes from "./routes/servicesRoutes.js";
 
 //Configurar la app
 const app = express();
 
 // Definir una ruta
-app.get("/", (req, res) => {
-  const product = {
-    id: 1,
-    price: 30,
-    name: "laptop",
-  };
-  res.json(product);
-});
+app.use("/api/services", servicesRoutes);
 
 // Definir puerto
 const PORT = process.env.PORT || 4000;
